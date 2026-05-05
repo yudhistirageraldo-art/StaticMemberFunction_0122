00e86ae2d571ca0e16583bbde2d67553683ba281
yudhistirageraldo <yudhistirageraldo@gmail.com>
2026-05-05 19:59:26 +0700

    int main

--- destructor.cpp
+++ destructor.cpp
@@ -43,3 +43,11 @@ void angka::isidata()
     }
     cout << endl;
 }
+int main()
+{
+    angka belajarcpp(3);                      //constructor dipanggil
+    angka *ptrBelajarcpp = new angka(5);     //constructor dipanggil
+    delete ptrBelajarcpp;                   //destructor dipanggil
+
+    return 0;
+}
