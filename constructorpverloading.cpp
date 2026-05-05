29ddbd26a03114980ba50ed7d77e426da5fa3b0d
yudhistirageraldo <yudhistirageraldo@gmail.com>
2026-05-05 19:56:16 +0700

    mahasiswa iNim

--- constructorpverloading.cpp
+++ constructorpverloading.cpp
@@ -23,17 +23,4 @@ mahasiswa::mahasiswa(int iNim) {
     nim = iNim; // definisi hanya NIM
 }
 
-mahasiswa::mahasiswa(string iNama) {
-    nama = iNama; // definisi hanya NAMA
-}
-
-mahasiswa::mahasiswa(int iNim, string iNama) {
-    nim = iNim; // definisi NIM dan NAMA
-    nama = iNama;
-}
-
-void mahasiswa::cetak() {
-    cout << endl << "Nim = " << nim << endl;
-    cout << "Nama = " << nama << endl;
-}
 
