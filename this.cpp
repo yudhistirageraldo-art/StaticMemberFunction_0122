a995f5788cea2b4567b2d434c287c7da54a830ef
yudhistirageraldo <yudhistirageraldo@gmail.com>
2026-05-05 19:53:11 +0700

    class buku

--- this.cpp
+++ this.cpp
@@ -1,3 +1,12 @@
 #include <iostream>
 using namespace std;
 
+class buku{
+    string judul;
+    public:
+        string setget(string judul){
+            this->judul = judul; // nilai parameter 'judul' untuk member variabel 'judul'
+            return this->judul; // return variabel judul.
+        }
+};
+
