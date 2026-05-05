8aa8aff05d912b16bd70b09a1ef781faf8c70603
yudhistirageraldo <yudhistirageraldo@gmail.com>
2026-05-05 19:59:14 +0700

    void angka

--- destructor.cpp
+++ destructor.cpp
@@ -25,3 +25,21 @@ angka::~angka(){ //destructor
     delete[] arr;
     cout << "Alamat Arr Sudah Dilepaskan" << endl;
 }
+
+void angka::cetakdata()
+{
+    for (int i = 1; i <= panjang; i++)
+    {
+        cout << i << " = " << arr[i] << endl;
+    }
+}
+
+void angka::isidata()
+{
+    for (int i = 1; i <= panjang; i++)
+    {
+        cout << i << " = ";
+        cin >> arr[i];
+    }
+    cout << endl;
+}
